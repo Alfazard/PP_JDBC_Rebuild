443ae83ca1ea01a82e8dfcaf8425d53f97da10d6
Alfazard <alfazard@rambler.ru>
2023-05-05 15:40:17 +0300

    Revert "Task complete"
    This reverts commit dc0b2c8dbb305ca9aa6ab1dc661a6cb5b299c3d8.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,30 +1,7 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.service.UserService;
-import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import static jm.task.core.jdbc.util.Util.closePool;
-
 public class Main {
     public static void main(String[] args) {
-        try{
-            Util.getConnect();
-            UserService service = new UserServiceImpl();
-
-            service.createUsersTable();
-
-            service.saveUser("Name1", "LastName1", (byte) 20);
-            service.saveUser("Name2", "LastName2", (byte) 25);
-            service.saveUser("Name3", "LastName3", (byte) 31);
-            service.saveUser("Name4", "LastName4", (byte) 38);
-
-            service.removeUserById(1);
-            service.getAllUsers().forEach (System.out::println);
-        service.cleanUsersTable();
-        service.dropUsersTable();
-          } finally {
-            closePool();
-        }
+        // реализуйте алгоритм здесь
     }
 }
